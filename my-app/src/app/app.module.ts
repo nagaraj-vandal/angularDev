6febf7e6111a76b3a00357e218f27569ab510157
nagaraj-vandal <nagarajvandal@gmail.com>
2021-11-08 18:23:09 +0530

    new changes in angular app

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { ServerComponent } from './server/server.component';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
